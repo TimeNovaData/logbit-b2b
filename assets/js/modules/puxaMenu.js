e59bb1bfaee0e785320e36323de1609d09b9be2a
Adr <senhoraroldo@gmail.com>
2021-11-05 10:57:28 -0300

    in progress

--- assets/js/modules/puxaMenu.js
+++ assets/js/modules/puxaMenu.js
@@ -1,6 +1,6 @@
 export default async function puxaMenu() {
 	const menuContainer = document.querySelector('.menu--container');
-	const resposta = await fetch("menu.html")
+	const resposta = await fetch("./menu.html")
 	const html = await resposta.text()
 	menuContainer.innerHTML = html
 
