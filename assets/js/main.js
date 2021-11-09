c736041b53b70d7029405b74f453f7a22c925d3f
Adr <senhoraroldo@gmail.com>
2021-11-09 13:09:58 -0300

    animaçaoi

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ if (pageHome) {
 	mudaAtivoNoHover('.two--item')
 	calendar()
 
-	new Swiper('.one .swiper', swiperHome)
+	// new Swiper('.one .swiper', swiperHome)
 	new Swiper('.five--swiper', swiperTalks)
 	mobile ? new Swiper('.six--swiper', swiperTalks) : ""
 
@@ -60,3 +60,51 @@ if (pageHome) {
 document.addEventListener("scroll", menuPreenchido);
 document.addEventListener("DOMContentLoaded", () => body.classList.add("DOMContentLoaded"))
 
+
+
+var cerchio = document.querySelectorAll('.magnetic');
+
+cerchio.forEach(function (elem) {
+	$(document).on('mousemove touch', function (e) {
+		magnetize(elem, e);
+	});
+})
+function magnetize(el, e) {
+	var mX = e.pageX,
+		mY = e.pageY;
+	const item = $(el);
+
+	const customDist = item.data('dist') * 5 || 80;
+	const centerX = item.offset().left + (item.width() / 2);
+	const centerY = item.offset().top + (item.height() / 2);
+
+	var deltaX = Math.floor((centerX - mX)) * -0.45;
+	var deltaY = Math.floor((centerY - mY)) * -0.45;
+
+	var distance = calculateDistance(item, mX, mY);
+
+	if (distance < customDist) {
+		gsap.to(item, 1, {
+			y: deltaY,
+			x: deltaX,
+			scale: 1.05
+		});
+		item.addClass('magnet');
+	} else {
+		gsap.to(item, 0.6, {
+			y: 0,
+			x: 0,
+			scale: 1
+		});
+		item.removeClass('magnet');
+	}
+}
+
+function calculateDistance(elem, mouseX, mouseY) {
+	return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left + (elem.width() / 2)), 2) + Math.pow(mouseY - (elem.offset().top + (elem.height() / 2)), 2)));
+}
+
+
+function lerp(a, b, n) {
+	return (1 - n) * a + n * b
+}
